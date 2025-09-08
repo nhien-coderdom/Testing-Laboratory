10088e4d31c617e4bdb48f705b9c8ddf285538be
nhin <117671495+nhien-coderdom@users.noreply.github.com>
2025-09-08 08:46:46 +0000

    lab1 b

---
+++ lab1/lab1_b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int f1(int x) {
+    if (x > 10)
+        return 2 * x;
+    else if (x > 0)
+        return -x;
+    else
+        return 2 * x;
+}
+
+int main()
+{
+    int T; 
+    cin >> T;     
+    for (int i = 0; i < T; i++) {
+        int x;
+        cin >> x;
+        cout << f1(x) << endl;
+    }
+    return 0;
+}
